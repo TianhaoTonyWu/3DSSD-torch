f95d9cd89a9ca711bf8d42809f0bacd2b33ced78
TianhaoTonyWu <tianhao.wu@mail.mcgill.ca>
2022-07-15 10:13:17 -0400

    Update kitti_dataset.py

--- pcdet/datasets/kitti/kitti_dataset.py
+++ pcdet/datasets/kitti/kitti_dataset.py
@@ -188,7 +188,7 @@ class KittiDataset(DatasetTemplate):
 
         sample_id_list = sample_id_list if sample_id_list is not None else self.sample_id_list
         with futures.ThreadPoolExecutor(num_workers) as executor:
-            infos = executor.map(process_single_scene(), sample_id_list)
+            infos = executor.map(process_single_scene, sample_id_list)
         return list(infos)
 
     def create_groundtruth_database(self, info_path=None, used_classes=None, split='train'):
